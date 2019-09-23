4d3739e069d08f9642fe12b97958cc2f8c053d3b
squee292darkside <Sered7896>
2019-09-23 17:45:44 -0500

    inital commit

---
+++ src/main/java/com/example/timesheet2/Timesheet2Application.java
@@ -0,0 +1,13 @@
+package com.example.timesheet2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Timesheet2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Timesheet2Application.class, args);
+	}
+
+}
---
+++ src/main/java/forms/Users.java
@@ -0,0 +1,50 @@
+package forms;
+
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Users {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max = 15)
+    private String name;
+
+    @NotNull
+    @Size(min = 3, max = 15)
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
